0cd7b20b48c5415cb9839bfc4fd7ba0fd2d1fb51
obiuno <fifart@gmail.com>
2017-06-16 07:59:47 +0000

    Paperclip Gem added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614085604) do
+ActiveRecord::Schema.define(version: 20170616072051) do
 
   create_table "hotels", force: :cascade do |t|
     t.string   "name"
@@ -28,8 +28,12 @@ ActiveRecord::Schema.define(version: 20170614085604) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
